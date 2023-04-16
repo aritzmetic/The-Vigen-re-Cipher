7d2612661f91d6d19799aabb25c59d2f69e1f43d
unknown <arixxdelera21@gmail.com>
2023-04-16 12:28:44 +0800

    pseudocode has been added

---
+++ the vigenre cipher.py
@@ -0,0 +1,17 @@
+# Delera, Aritz B.
+# The Vigenere Code Cipher
+
+# Define a class for Vigenere Cipher
+# create a dictionary to change the alphabets to numbers
+# convert the key to numerical values
+# Add the character to the ciphertext without encrypting it if it is not an alphabet.
+# Determine each letter's numerical value in the message.
+# Calculate the result modulo 26 by adding the total number of the alphabet in the key.
+# Add the alphabet to the ciphertext after converting the encrypted numerical value to a alphabet.
+# Move to the next alphabet in the key.
+# Use while loop for a condition-controlled loop
+# Ask the name of the user to create a greeting
+# ask the user to input the message and key
+# Create an instance of VigenereCipher class and encrypt the message using the key
+# Display the encrypted message
+# Ask the user if they want to continue or exit
